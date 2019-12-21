1af477136a3d66392592e28897756fea04e34b43
jayin92 <jayin920805@gmail.com>
2019-12-21 23:28:43 +0800

    Scrapy

---
+++ rust-tutorial/armstrong.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let mut v = vec![0, 0];
+    v.push(1);
+    v.push(2);
+    for x in 0..v.len(){
+        println!("{}", v[x]);
+    }
+}         
+
+println!("length is {}", vec1.len());
+for x in 0..vec1.len() {
+    println!("{} {}", x, vec1[x]);
+}
+}
